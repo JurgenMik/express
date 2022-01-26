4eafa3e6916efd46158af222e80fd918a9f95bca
Jurgen Mikk <jurgen.mikk@khk.ee>
2022-01-26 12:26:21 +0200

    Basic use of ejs

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const express = require ('express')
-const {response} = require("express");
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
 
-app.get('/', (req,res)=> {
-    res.send('<a href="/contact"> Contact us </a> <br> <a href="/about"> About us </a>');
+app.get('/user/:username', (req, res)=>{
+    // get parameter data from addressrow
+    let user =req.params.username;
+    // use data in template
+    res.render('index.ejs', {username:user});
 });
-app.get('/contact',(req,res)=>{
-    res.send( '<h1> About Us page </h1>');
-})
 
-app.get('/about',(req,res)=>{
-    res.send('<h1> About Us page </h1>');
-})
-
-app.get('*',(req,res)=>{
-    res.send('404. This page does not exist. <a href="/"> Go to Homepage </a>')
-})
-
-// listen to app via port 3000
-
-app.listen(3000, ()=>{
+app.listen(3000,()=>{
     console.log('Server started on http://localhost:3000');
 });
